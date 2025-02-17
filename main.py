f9e5a05b3db1a85d3936c9620887cd4c59f41260
denizberkin <berkindeniz2000@gmail.com>
2025-02-17 16:30:06 +0300

    random changes in main to testi

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from copy import deepcopy
 import joblib
 
+import datasets
+
 import numpy as np
 import pandas as pd
 import lightgbm as lgbm 
@@ -9,17 +11,17 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import ndcg_score
 
-from utils.load import load_df
+from utils.load import load_df, read_combine_parquets, p2csv
 from utils.preprocess import scoring_function, cosine_sim
 
 pd.options.mode.chained_assignment = None
 
 
 # XXX: use smoothed labels?, use scaled scoring?
-SCORE_MAP = {"E": 3.0,  # exact
-           "S": 2.0,  # substitute
-           "C": 1.0,  # complementary
-           "I": 0.0}  # irrelevant
+SCORE_MAP = {"Exact": 1.0,  # exact
+           "Substitute": 0.1,  # substitute
+           "Complement": 0.01,  # complementary
+           "Irrelevant": 0.0}  # irrelevant
 
 
 def pipeline(df: pd.DataFrame
@@ -63,18 +65,19 @@ def pipeline(df: pd.DataFrame
     learning_rate=0.05
     )
     
-    lgbm_ranker.fit(x_train, y_train,
-                    eval_metric="ndcg",
-                    eval_set=[(x_val, y_val)],
-                    group=train_groups,
-                    eval_group=[val_groups])
+    # lgbm_ranker.fit(x_train, y_train,
+    #                 eval_metric="ndcg",
+    #                 eval_set=[(x_val, y_val)],
+    #                 group=train_groups,
+    #                 eval_group=[val_groups])
     
-    y_test_pred = lgbm_ranker.predict(x_test)
-    ndcg_score_test = ndcg_score(y_test, y_test_pred)
-    print("NDCG on test: ", ndcg_score_test)
+    # y_test_pred = lgbm_ranker.predict(x_test)
+    # ndcg_score_test = ndcg_score(y_test, y_test_pred)
+    # print("NDCG on test: ", ndcg_score_test)
 
     joblib.dump(lgbm_ranker, "ranker_model.pkl")
     return lgbm_ranker
+
 
 
 def main(**kwargs):
@@ -88,28 +91,47 @@ def main(**kwargs):
     # print(df_sources.head())
     
     # join examples and products on product_id
-    df_joined = pd.merge(df_examples,
-                         df_products,
-                         how="left",
-                         left_on=["product_locale", "product_id"],
-                         right_on=["product_locale", "product_id"])
-    
-    # print(df_joined.columns)
 
-    print(df_joined.columns)
-    df_joined_small = df_joined[df_joined["small_version"] == 1
-                                and df_joined["product_locale"] == "us"]
+    # pipeline(df_joined_small[:5000])
 
-    # print(df_joined_small.sample(1))
 
-    pipeline(df_joined_small[:5000])
 
 
 if __name__ == "__main__":
-    ROOT = os.path.join(os.getcwd(), "shopping_queries")
+    ROOT = os.path.join(os.getcwd(), "formatted_esci")
+    CSV_ROOT = os.path.join(os.getcwd(), "formatted_csv")
 
-    examples = os.path.join(ROOT, "dataset_examples.parquet")
-    products = os.path.join(ROOT, "dataset_products.parquet")
-    sources = os.path.join(ROOT, "dataset_sources.csv")
+    # examples = os.path.join(ROOT, "dataset_examples.parquet")
+    # products = os.path.join(ROOT, "dataset_products.parquet")
+    # sources = os.path.join(ROOT, "dataset_sources.csv")
+    
+    df = pd.read_csv("formatted_csv/train-00000_sample1000.csv")
 
-    main(ex=examples, pr=products, src=sources)
+    # convert relevance to score 
+    df["relevance"] = df["esci_label"].apply(scoring_function,
+                                                    args=(SCORE_MAP,))
+    
+    print(df["product_id"].value_counts())
+    
+    vectorizer = TfidfVectorizer(max_features=300)
+    df["query_embed"] = list(vectorizer.fit_transform(df['query']).toarray())
+    df["title_embed"] = list(vectorizer.transform(df["product_title"]).toarray())
+
+    print(df["query_embed"][0])
+
+    
+    """
+    filenames = os.listdir(ROOT)
+    train_filenames = [os.path.join(ROOT, file) for file in filenames if file.startswith("train")]
+    test_filenames = [os.path.join(ROOT, file) for file in filenames if file.startswith("test")]
+
+    dataset = read_combine_parquets(train_filenames[:2], t_return="list")
+
+    col2drop = ["product_bullet_point"]
+    p2csv(dataset[0], os.path.join(CSV_ROOT, "train-00000_sample1000.csv"), columns_to_drop=col2drop, get_small_us=True, idx=1000)
+    """
+    # main(ex=examples, pr=products, src=sources)
+
+    # ds = datasets.load_dataset("tasksource/esci")
+    # print(ds)
+    print()  # 
