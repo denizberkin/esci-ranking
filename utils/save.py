5afed67e9cd3911344ac2f23f83fcffb2c97ab33
denizberkin <berkindeniz2000@gmail.com>
2025-02-26 16:50:50 +0300

    fix naming

--- utils/save.py
+++ utils/save.py
@@ -5,21 +5,21 @@ import joblib
 from utils.variables import EMBEDDING_FOLDER
 
 
-def save_embeddings2npy(embeddings: dict):
+def save_embeddings2npy(embeddings: dict, fn: str = ""):
     """ input query and feature embeddings as dict, save them to EMBEDDING_FOLDER"""
     if not os.path.exists(EMBEDDING_FOLDER):
         os.makedirs(EMBEDDING_FOLDER)
     
-    np.save(os.path.join(EMBEDDING_FOLDER, "query_embeddings.npy"), embeddings["query"])
-    np.save(os.path.join(EMBEDDING_FOLDER, "feature_embeddings.npy"), embeddings["feature"])
+    np.save(os.path.join(EMBEDDING_FOLDER, f"{fn}_query_embeddings.npy"), embeddings["query"])
+    np.save(os.path.join(EMBEDDING_FOLDER, f"{fn}_feature_embeddings.npy"), embeddings["feature"])
 
 
-def load_embeddings() -> tuple[np.ndarray, np.ndarray]:
+def load_embeddings(fn: str = "") -> tuple[np.ndarray, np.ndarray]:
     """ load embeddings from EMBEDDING_FOLDER """
-    query_embeddings = np.load(os.path.join(EMBEDDING_FOLDER, "query_embeddings.npy"), 
+    query_embeddings = np.load(os.path.join(EMBEDDING_FOLDER, f"{fn}_query_embeddings.npy"), 
                                allow_pickle=True
                                )
-    feature_embeddings = np.load(os.path.join(EMBEDDING_FOLDER, "feature_embeddings.npy"), 
+    feature_embeddings = np.load(os.path.join(EMBEDDING_FOLDER, f"{fn}_feature_embeddings.npy"), 
                                  allow_pickle=True
                                  )
     return (query_embeddings, feature_embeddings)
