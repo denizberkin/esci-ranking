0798b616a41a2eeedd096f3fdeec8bfb9a30de64
denizberkin <berkindeniz2000@gmail.com>
2025-02-26 16:51:00 +0300

    some cleanup

---
+++ inference.py
@@ -0,0 +1,28 @@
+import os
+
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+import lightgbm as lgbm
+
+from utils.save import load_model
+from utils.variables import MODEL_SAVE_FOLDER
+from utils.plot import plot_importances
+
+
+def test(model: lgbm.Booster = None, 
+          test_df: pd.DataFrame = None,
+          feature_columns: list[str] = None):
+    if model is None:
+        model: lgbm.Booster = load_model(os.path.join(MODEL_SAVE_FOLDER, "model_fold_2.pkl"))
+    
+    plot_importances(model)
+
+    # TODO: testing logic, metrics etc...
+
+
+
+
+if __name__ == "__main__":
+    test()
