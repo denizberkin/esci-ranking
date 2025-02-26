023b80b623b763823e45b22fae98e376708f9930
denizberkin <berkindeniz2000@gmail.com>
2025-02-26 11:13:10 +0300

    plot folder, relocations etc

--- utils/variables.py
+++ utils/variables.py
@@ -1,9 +1,14 @@
 import os
 
+
+PLOT_FOLDER = "plots/"
+
+ST_MODEL_NAME = "all-MiniLM-L6-v2"
 MODEL_SAVE_FOLDER = "models/"
 ROOT_FOLDER = "formatted_esci/"
 EMBEDDING_FOLDER = "embeddings/"
 NUM_THREADS = 6  # if no saved embeddings and cuda not available
+
 
 COLUMNS_TO_PROCESS =  [  # "query",    # data leak? 
                        "product_title", 
@@ -12,7 +17,6 @@ COLUMNS_TO_PROCESS =  [  # "query",    # data leak?
                        "product_color"]  # product_text is already a combined version
 
 
-# XXX: use smoothed labels?, use scaled scoring?
 SCORE_MAP = {
     "Exact": 3,  # exact
     "Substitute": 2,  # substitute
@@ -21,8 +25,7 @@ SCORE_MAP = {
     }
 
 
-ST_MODEL_NAME = "all-MiniLM-L6-v2"
-
+# lgbm params
 MODEL_PARAMS = {
     "objective": "lambdarank",
     "metric": "ndcg",
@@ -32,8 +35,8 @@ MODEL_PARAMS = {
     "verbose": -1
     }
 
-COLOURS_TXT_FN = "unique_colours.txt"
 
+COLOURS_TXT_FN = "unique_colours.txt"
 with open(COLOURS_TXT_FN, "r+") as f:
     COLOURS = list(set(f.read().splitlines()))
     
