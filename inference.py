418ba968b8b0a3e95bb907cf940d70e56f066142
b84401071 <berkin.deniz.kahya@h-partners.com>
2025-03-02 20:56:02 +0300

    result comments

--- inference.py
+++ inference.py
@@ -20,6 +20,25 @@ def test(model: lgbm.Booster = None,
     plot_importances(model)
 
     # TODO: testing logic, metrics etc...
+    """
+    NUM FEATURES:  2 - only cossim (tfidf, sentence transformer)
+    Processing Fold 1
+    Validation Fold 1 - NDCG: 0.9105, Kendall Tau: nan, Weighted Tau: nan
+    Processing Fold 2
+    Validation Fold 2 - NDCG: 0.9110, Kendall Tau: nan, Weighted Tau: nan
+    Processing Fold 3
+    Validation Fold 3 - NDCG: 0.9072, Kendall Tau: nan, Weighted Tau: nan
+    Processing Fold 4
+    Validation Fold 4 - NDCG: 0.9091, Kendall Tau: nan, Weighted Tau: nan
+    Processing Fold 5
+    Validation Fold 5 - NDCG: 0.9080, Kendall Tau: nan, Weighted Tau: nan
+
+    Metrics of Best Model - Fold 2: NDCG: 0.9110, Kendall Tau: nan, Weighted Tau: nan
+    """
+
+    """
+    
+    """
 
 
 
