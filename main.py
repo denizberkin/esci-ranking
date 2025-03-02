8fa0c9d4792d0c263297f1e7694aad7a481b4db9
b84401071 <berkin.deniz.kahya@h-partners.com>
2025-03-02 20:57:01 +0300

    --

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from inference import test
 from train import train
 
 from utils.logger import log_time
-from utils.load import load_df
+from utils.save import load_df
 from utils.plot import plot_df_corr, plot_column_skewness
 from pipeline import preprocess_pipeline
 
@@ -26,15 +26,15 @@ if __name__ == "__main__":
     train_filenames = [f for f in os.listdir(ROOT_FOLDER) if f.startswith("train")]
     test_filenames = [f for f in os.listdir(ROOT_FOLDER) if f.startswith("test")]
 
-    df = load_df(train_filenames[: 2])
-    df_test = load_df(test_filenames[:])
+    df = load_df(train_filenames[:1])  # no effect, saved the processed df and loading in "pipeline"
+    df_test = load_df(test_filenames[:1])
 
     print("DF COLUMNS: ", df.columns)
     print("DF TEST COLUMNS: ", df_test.columns)
     print("DF SHAPE: ", df.shape)
     print("DF TEST SHAPE: ", df_test.shape)
 
-    df = df[: 1000]  # for testing purposes
+    # df = df[: 1000]  # for testing purposes
 
     df, feature_columns = preprocess_pipeline(df)
     # df_test = preprocess_pipeline(df_test)
@@ -42,6 +42,9 @@ if __name__ == "__main__":
     plot_df_corr(df[feature_columns])
     plot_column_skewness(df[feature_columns])
 
+    print("NUM FEATURES: ", len(feature_columns))
     best_model = train(df, feature_columns)
     
-    test(best_model, df_test, feature_columns)
+    test(best_model
+        test_df=df_test, 
+        feature_columns=feature_columns)
